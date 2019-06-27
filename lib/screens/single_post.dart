3087f7048e626e3cf4900e7983027e0f6b67490c
karrar saad <kararmax124@gmail.com>
2019-06-27 13:26:48 -0700

    test

---
+++ lib/screens/single_post.dart
@@ -0,0 +1,32 @@
+
+import 'package:untitled55/models/post.dart';
+import 'package:flutter/material.dart';
+
+class Singlepost extends StatefulWidget {
+
+ final Post post;
+
+ Singlepost(this.post);
+
+  @override
+  _SinglepostState createState() => _SinglepostState();
+}
+
+class _SinglepostState extends State<Singlepost> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: Container(
+        child: Text(''),
+
+
+        color: Colors.deepOrange
+      ),
+
+
+    );
+  }
+}
